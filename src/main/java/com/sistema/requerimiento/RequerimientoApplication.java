12cfb31c8f80ddec73659dd54fe7eaa135c26de9
octaviomunoz <ostavio09@gmail.com>
2020-09-28 19:33:03 -0300

    primer commit

---
+++ src/main/java/com/sistema/requerimiento/RequerimientoApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.requerimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequerimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequerimientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/requerimiento/RequerimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.requerimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequerimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
